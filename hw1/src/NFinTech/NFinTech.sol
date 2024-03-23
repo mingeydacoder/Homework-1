6558ac81d640f21ce6735ab1c1734c04d4875670
4ll3nch3n <allenchen0121@gmail.com>
2024-03-23 23:55:51 +0800

    03/23

--- hw1/src/NFinTech/NFinTech.sol
+++ hw1/src/NFinTech/NFinTech.sol
@@ -75,23 +75,44 @@ contract NFinTech is IERC721 {
     }
 
     function setApprovalForAll(address operator, bool approved) external {
-        // TODO: please add your implementaiton here
+        require(msg.sender != operator);
+        _operatorApproval[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
     }
 
     function isApprovedForAll(address owner, address operator) public view returns (bool) {
-        // TODO: please add your implementaiton here
+            return _operatorApproval[owner][operator];
     }
 
     function approve(address to, uint256 tokenId) external {
-        // TODO: please add your implementaiton here
+        address owner = ownerOf(tokenId);
+        require(to != owner);
+        _tokenApproval[tokenId] = to;
+        emit Approval(owner, to, tokenId);
     }
 
     function getApproved(uint256 tokenId) public view returns (address operator) {
-        // TODO: please add your implementaiton here
+        require(_owner[tokenId] != address(0));
+        return _tokenApproval[tokenId];
     }
 
     function transferFrom(address from, address to, uint256 tokenId) public {
-        // TODO: please add your implementaiton here
+        address owner = ownerOf(tokenId);
+        require(msg.sender == owner || msg.sender == getApproved(tokenId) || isApprovedForAll(owner, msg.sender));
+        require(from == owner);
+        require(to != address(0));
+
+        // Clear approval
+        _tokenApproval[tokenId] = address(0);
+
+        // Update balances
+        _balances[from] -= 1;
+        _balances[to] += 1;
+
+        // Transfer ownership
+        _owner[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) public {
